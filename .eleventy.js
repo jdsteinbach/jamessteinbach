b5dec8693478b302fe427bbdaf56412c3ed7371e
James Steinbach <jdsteinbach@gmail.com>
2026-01-18 18:29:24 -0700

    cfp

--- .eleventy.js
+++ .eleventy.js
@@ -1,36 +1,40 @@
-const markdownIt = require('markdown-it')
+const markdownIt = require("markdown-it");
 
-module.exports = eleventyConfig => {
+module.exports = (eleventyConfig) => {
   // Markdown
-  eleventyConfig.setLibrary('md', markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true
-  }))
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({
+      html: true,
+      breaks: true,
+      linkify: true,
+      typographer: true,
+    }),
+  );
 
   // Passthrough files
-  eleventyConfig.addPassthroughCopy('src/pdf')
-  eleventyConfig.addPassthroughCopy('src/_redirects')
-  eleventyConfig.addPassthroughCopy('src/img')
-  eleventyConfig.addPassthroughCopy('src/fonts')
-  eleventyConfig.addPassthroughCopy('src/favicon.ico')
-  eleventyConfig.addPassthroughCopy('src/robots.txt')
-  eleventyConfig.addPassthroughCopy('src/humans.txt')
-  eleventyConfig.addPassthroughCopy('src/tile-wide.png')
-  eleventyConfig.addPassthroughCopy('src/tile.png')
-  eleventyConfig.addPassthroughCopy('src/favicon.ico')
-  eleventyConfig.addPassthroughCopy('src/favicon.ico')
+  eleventyConfig.addPassthroughCopy("src/pdf");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/media");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/humans.txt");
+  eleventyConfig.addPassthroughCopy("src/tile-wide.png");
+  eleventyConfig.addPassthroughCopy("src/tile.png");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   // Watch
-  eleventyConfig.addWatchTarget('src/sass/**/*.scss')
+  eleventyConfig.addWatchTarget("src/sass/**/*.scss");
 
   return {
     dir: {
-      input: 'src',
-      output: '_site'
+      input: "src",
+      output: "_site",
     },
-    templateFormats: ['liquid', 'md', '11ty.js'],
-    passthroughFileCopy: true
-  }
-}
+    templateFormats: ["liquid", "md", "11ty.js"],
+    passthroughFileCopy: true,
+  };
+};
